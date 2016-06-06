5b88db7a6027b091bbfdc6108bf2ebb3ac07680d
David Gardiner <david@gardiner.net.au>
2016-06-06 15:02:21 +0930

    Add WPF test project for White demo

---
+++ IntelliTestAndUnitTesting.White/BarryTests.cs
@@ -0,0 +1,31 @@
+﻿using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.WindowsAPI;
+using Xunit;
+
+namespace IntelliTestAndUnitTesting.White
+{
+    public class BarryTests
+    {
+        [Fact]
+        public void ChoosingThreeFromList_EnablesButton()
+        {
+            
+            var applicationPath = @"..\..\..\IntelliTestAndUnitTesting.WhiteWpfApp\bin\Debug\IntelliTestAndUnitTesting.WhiteWpfApp.exe";
+            Application application = Application.Launch(applicationPath);
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            var button = window.Get<Button>(SearchCriteria.ByAutomationId("MyButton"));
+            var list = window.Get<ComboBox>(SearchCriteria.ByAutomationId("MyList"));
+
+            list.KeyIn(KeyboardInput.SpecialKeys.DOWN);
+            list.KeyIn(KeyboardInput.SpecialKeys.DOWN);
+
+            Assert.True(button.Enabled);
+        }
+    }
+}
---
+++ IntelliTestAndUnitTesting.WhiteWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntelliTestAndUnitTesting.WhiteWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi there!");
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ListBoxItem)comboBox.SelectedValue).Content.ToString() == "Three")
+                button.IsEnabled = true;
+        }
+    }
+}
